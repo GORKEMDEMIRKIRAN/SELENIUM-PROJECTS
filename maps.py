e3ea6db1a005b43d03f9d31d6b2dcfca85ab9d25
GÖRKEM-DEMİRKIRAN <gorkemgorkem1998@gmail.com>
2024-07-03 13:25:50 +0300

    Functions were created for distance and duration information.

--- maps.py
+++ maps.py
@@ -41,6 +41,76 @@ driver_edge.get("https://www.google.com/maps")
 
 
 
+# Bu fonksiyon maps  mevcut olan yol tarifi butonuna tıklamaktadır.
+def button_click():
+    # Bu kısımda haritalardaki yol tarifi butununa tıklıyor.
+    way_button=driver_edge.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[8]/div[3]/div[1]/div[1]/div/div[1]/div[2]/button")
+    #way_button = WebDriverWait(driver_edge, 60).until(expected_conditions.visibility_of_element_located((By.XPATH,"/html/body/div[1]/div[3]/div[8]/div[3]/div[1]/div[1]/div/div[2]/div[2]/button")))
+    #way_button=driver_edge.find_element(By.ID,"hArJGc")
+    way_button.click()
+
+# Verilen iki nokta arasındaki süre ve km bilgisini oluşturur.
+def two_point_enter(first_point,second_point):
+    # BU VERİLEN İKİ NOKTA ATASINDAKİ MESAFENİN SAAT VE KM CİNSİNDEN DEĞERİNİ VERİR.
+    # girilen 2 nokta ismini "string" olarak çeviriyor.
+    first_point=str(first_point)
+    second_point=str(second_point)
+  
+    # ilk nokta ve ikinci nokta olarak girilen kutuların "xpath" bağlantılarını buluyor.
+    first_length = WebDriverWait(driver_edge, 60).until(expected_conditions.visibility_of_element_located((By.XPATH,"/html/body/div[1]/div[3]/div[8]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/input")))
+    second_length=driver_edge.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[8]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/div[1]/div/input")
+    # ilk noktayı giriyor ve aratıyor.
+    # ikinci noktayıda giriyor ve aratıyor.
+    
+    first_length.send_keys(first_point)
+    second_length.send_keys(second_point)
+    second_length.send_keys(Keys.ENTER)
+    
+    
+    #  Bulunamayan değerler için saat ve mesafe bilgilerine 0 değerini atadık.
+    try:
+        error_message = WebDriverWait(driver_edge, 1).until(
+            expected_conditions.visibility_of_element_located((
+            By.XPATH, "/html/body/div[1]/div[3]/div[8]/div[3]/div[1]/div[2]/div/div[4]/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div[2]/a")))
+            
+        print(f"Hata: {first_point} ve {second_point} arasında sonuç bulunamadı.")
+        return f"Hata mesajı: {first_point} ve {second_point} arasında sonuç bulunamadı."
+    except TimeoutException:
+        pass
+
+    # Saat ve mesafe bilgisini birlikte alıyor.
+    try:
+        # Saati bilgisinin hmtl uzantısı
+        hours = WebDriverWait(driver_edge, 10).until(
+                expected_conditions.visibility_of_element_located((    
+                By.XPATH, "/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/div[1]/div/div[1]/div[1]")))
+        
+        hours_info = hours.text
+    except TimeoutException:
+        print(f"Saat bilgisi bulunamadı: {first_point} ve {second_point}")
+        hours_info = 0
+
+    try:
+        # Km bilgisinin html uzantısı
+        distance = WebDriverWait(driver_edge, 10).until(
+                expected_conditions.visibility_of_element_located((
+                By.XPATH, "/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/div[1]/div/div[1]/div[2]")))
+        
+        distance_info = distance.text
+    except TimeoutException:
+        print(f"Mesafe bilgisi bulunamadı: {first_point} ve {second_point}")
+        distance_info = 0
+        
+    
+    # ilk nokta - ikinci nokta - saat bilgisi - mesafe bilgisi
+    total = [first_point, second_point, hours_info, distance_info]
+    # ilk ve ikinci hücreyi temizler.
+    first_length.clear()
+    second_length.clear()
+
+    return total
+
+
 time.sleep(10)
 
 driver_edge.quit()
